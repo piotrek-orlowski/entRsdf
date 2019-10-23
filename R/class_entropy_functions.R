27c6631b691383c199bad1e3fcbf98e0891502e9
Piotr Orłowski <piotrek.orlowski@gmail.com>
2019-10-23 14:12:14 -0400

    added function for 2-level optimisation in AG2012 JoEmetrics

--- R/class_entropy_functions.R
+++ R/class_entropy_functions.R
@@ -20,7 +20,7 @@ entropy_functions <- R6::R6Class("entropy_functions"
                                    description = "This is a generic class for entropy SDF functions"
                                  ))
 
-xlogx_functions <- R6::R6Class("xlogx_functions"
+et_functions <- R6::R6Class("et_functions"
                                , inherit = entropy_functions
                                , public = list(
                                  objective = function(theta_vector, return_matrix){
@@ -51,5 +51,73 @@ xlogx_functions <- R6::R6Class("xlogx_functions"
                                     }
                                )
                                , private = list(
-                                 description = "Functions for fitting and recovering minimum-KL-distance SDFs. \nThey correspond to setting power = 0 in the Cressie-Read function family."
+                                 description = "Functions for fitting and recovering exponential-tilting SDFs. \nThey correspond to setting power = 0 in the Cressie-Read function family."
                                ))
+
+distance_et_functions <- R6::R6Class("distance_et_functions"
+                                     , inherit = entropy_functions
+                                     , public = list(
+                                        objective = function(theta_vector, return_matrix, penalty_value, store_lambda = TRUE){
+                                          
+                                          # Check if there is a pre-estimated lambda (to get a starting value for the first optimisation)
+                                          if(is.null(private$lambda_opt)){
+                                            lambda_opt <- rep(1, ncol(return_matrix))
+                                          } else {
+                                            lambda_opt <- private$lambda_opt
+                                          }
+                                          
+                                          # Optimize the inner problem given theta
+                                          # These inner objective / gradient / Hessian are coded in entropic-distance-estimators.cpp
+                                          def_opts <- nloptr::nl.opts()
+                                          def_opts$algorithm <- "NLOPT_LD_LBFGS"
+                                          optimisation_result <- nloptr::nloptr(x0 = lambda_opt
+                                                                                , eval_f = et_distance_objective
+                                                                                , opts = def_opts
+                                                                                , theta_extended = theta_vector
+                                                                                , return_matrix = return_matrix
+                                                                                , mu_penalty = penalty_value)
+                                          
+                                          # Recover optimal decision
+                                          lambda_opt <- optimisation_result$solution
+                                          
+                                          # save lambda_opt
+                                          if(store_lambda){
+                                            private$lambda_opt <- lambda_opt 
+                                          }
+                                          
+                                          # Calculate gradient wrt theta
+                                          gradient_theta <- et_distance_theta_gradient(lambda_opt
+                                                                            , theta_vector
+                                                                            , return_matrix
+                                                                            , penalty_value)
+                                          
+                                          # Return objective value and gradient
+                                          # obj value has to switch sign, because we minimize dist for max dual,
+                                          # but the dual here was minimised (that's how solvers work)
+                                          # and we have to switch signs again
+                                          return(list(objective = - optimisation_result$objective
+                                                 , gradient = gradient_theta))
+                                        }
+                                        # , gradient = function(theta_vector, return_matrix, penalty_value){
+                                        #   
+                                        #   if(is.null(private$lambda_opt)){
+                                        #     self$objective(theta_vector, return_matrix, penalty_value)$objective
+                                        #     lambda_opt <- private$lambda_opt
+                                        #   } else {
+                                        #     lambda_opt <- private$lambda_opt
+                                        #   }
+                                        #   
+                                        #   res <- et_distance_theta_gradient(lambda_opt
+                                        #                                     , theta_vector
+                                        #                                     , return_matrix
+                                        #                                     , penalty_value)
+                                        #   return(res)
+                                        # }
+                                        , get_lambda_stored = function(){
+                                          private$lambda_opt
+                                        }
+                                     )
+                                     , private = list(
+                                        lambda_opt = NULL
+                                        , description = "Functions for fitting and recovering exponential-tilting SDFs. \nThey correspond to setting power = 0 in the Cressie-Read function family."
+                                     ))
