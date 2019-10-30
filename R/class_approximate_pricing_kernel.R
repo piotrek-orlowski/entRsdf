5ec9e240bae5e580341f6b4e5d97dcfa168a5ded
Piotr Orłowski <piotrek.orlowski@gmail.com>
2019-10-30 13:25:31 -0400

    corrected bug in evaluation of leverage criterion which was taking the wrong dimension

--- R/class_approximate_pricing_kernel.R
+++ R/class_approximate_pricing_kernel.R
@@ -367,7 +367,7 @@ window_cv_pricing_kernel <- R6::R6Class("window_cv_pricing_kernel"
                                               
                                               # make cluster and first check environment variable for its size
                                               if(is.na(as.numeric(Sys.getenv("NUM_CORES")))){
-                                                par_cluster <- parallel::makeCluster(detectCores(TRUE))  
+                                                par_cluster <- parallel::makeCluster(parallel::detectCores(TRUE))  
                                               } else {
                                                 par_cluster <- parallel::makeCluster(as.numeric(Sys.getenv("NUM_CORES")))
                                               }
@@ -580,9 +580,10 @@ window_lev_pricing_kernel = R6::R6Class("window_lev_pricing_kernel"
                                           cv_criterion = function(fold, return_df, coefficients_by_fold){
                                             # recover portfolio coefficients (matrix size of num assets x penalty par)
                                             loc_coefs <- coefficients_by_fold[[fold+1]]$theta_compact_matrix
+                                            
                                             # calculate excess leverage
                                             excess_leverage <- apply(X = loc_coefs
-                                                                     , MARGIN = 2L
+                                                                     , MARGIN = 1L
                                                                      , function(vec){
                                                                        (sum(abs(vec)) - private$maximum_leverage)^2
                                                                      })}
